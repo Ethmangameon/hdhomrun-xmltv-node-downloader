a9fa491e5d5db95e9fcfc24a6ea47b886834b192
Ethan Scott Hedden <ethanhedden0118@gmail.com>
2025-02-24 16:06:46 -0500

    More code comments

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import { promisify } from 'util';
 dotenv.config();
 
 try {
+
     // Obtain the latest json from discover.json
     let response = await axios({
         method: 'GET',
@@ -15,12 +16,18 @@ try {
 
     // Define a variable for the url of the xmltv data
     var xmltvurl = `https://api.hdhomerun.com/api/xmltv?DeviceAuth=${response.data.DeviceAuth}`
+
 } catch (err) {
-    console.log('An error occurred obtaining the DeviceAuth from your hdhomerun.')
-    console.log(`The Error that occured\n"${err}"`)
+
+    // Console log errors and helpfull Human Readable text possible issues.
+    console.log('An error occurred obtaining the DeviceAuth from your hdhomerun.');
+    console.log(`The Error that occured\n"${err}"`);
     process.exit(101);
-}
+
+};
+
 try {
+  
     // Define what to do with the final data
     let finishedDownload = promisify(stream.finished);
     let writer = fs.createWriteStream('./xmltv.xml');
@@ -31,16 +38,24 @@ try {
         url: xmltvurl,
         responseType: 'stream',
     });
-    console.log(`Downloading "xmltv.xml" from ${xmltvurl}`)
+
+    // Console log downloading to Acknowledge the script doing anything
+    console.log(`Downloading "xmltv.xml" from ${xmltvurl}`);
     // Take response and write it to file
     responsestream.data.pipe(writer);
     await finishedDownload(writer);
+
 } catch (err) {
-    console.log('An error occurred obtaining the data from the hdhomrun api.')
-    console.log('Or an error occured writing the file "./xmltv.xml".')
-    console.log(`The Error that occured\n"${err}"`)
+
+    // Console log errors and helpfull Human Readable text possible issues.
+    console.log('An error occurred obtaining the data from the hdhomrun api.');
+    console.log('Or an error occured writing the file "./xmltv.xml".');
+    console.log(`The Error that occured\n"${err}"`);
     process.exit(102);
-}
+
+};
+
+// Console log Goobye message to make sure the user knows the script is done executing.
 console.log('Please ensure your "xmltv.xml" file is intact and ready!')
 console.log('Goodbye! ✌️')
 process.exit(0);
