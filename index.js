7109bbda287b18e46ef2e4fe8a25fa1439c6b537
Ethan Scott Hedden <ethanhedden0118@gmail.com>
2025-02-25 10:59:48 -0500

    add some new .env variables

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 // Import Depends
 import * as fs from 'fs';
-import { Readable } from 'node:stream'
+import { Readable } from 'node:stream';
 import { finished } from 'node:stream/promises';
 process.loadEnvFile();
+
+const dest = process.env.DEST;
+function getBoolean(str) {
+     if (str === "true") { return true }; return false;
+    };
+const output = getBoolean(process.env.CONSOLE_OUTPUT);
 
 try {
 
     // Obtain the latest json from discover.json
-    let response = await fetch(`http://${process.env.LOCALIP}/discover.json`);
-    let responsejson = await response.json()
+    let response = await fetch(`http://${process.env.LOCAL_IP}/discover.json`);
+    let responsejson = await response.json();
+
+    // Only console log additional info if it is set in .env
+    if (output) {
     // Console log obtaining data to Acknowledge the script doing something
-    console.log(`Obtaining JSON data from http://${process.env.LOCALIP}/discover.json`);
+    console.log(`Obtaining JSON data from http://${process.env.LOCAL_IP}/discover.json`);
     console.log(`~~~ discover.json ~~~\n${JSON.stringify(responsejson)}\nEOF`);
+    };
 
     // Define a variable for the url of the xmltv data
     var xmltvurl = `https://api.hdhomerun.com/api/xmltv?DeviceAuth=${responsejson.DeviceAuth}`;
@@ -30,10 +40,13 @@ try {
     // Download the xmltv.xml file from the xmltvurl variables defined url
     let response = await fetch(xmltvurl); 
     await finished(Readable.fromWeb(response.body)
-    .pipe(fs.createWriteStream(`./xmltv.xml`)));
+    .pipe(fs.createWriteStream(dest)));
 
+    // Only console log additional info if it is set in .env
+    if (output) {
     // Console log downloading to Acknowledge the script doing anything
     console.log(`Downloading "xmltv.xml" from ${xmltvurl}`);
+    };
 
 } catch (err) {
 
@@ -45,7 +58,10 @@ try {
 
 };
 
+// Only console log additional info if it is set in .env
+if (output) {
 // Console log Goobye message to make sure the user knows the script is done executing.
 console.log('Please ensure your "xmltv.xml" file is intact and ready!');
 console.log('Goodbye! ✌️');
 process.exit(0);
+};
